b5c2b445f9dd111321773854c35b12e7be27588d
Ajay <anayakg1@gmail.com>
2024-11-20 15:45:19 +0530

    code optimization and formatting

--- health_monitor.py
+++ health_monitor.py
@@ -2,23 +2,21 @@ import requests
 import time
 from datetime import datetime
 
-# Function to monitor the API health
+#function to monitor the API health
 def check_api_health(api_url, max_response_time=2):
     try:
         response = requests.get(api_url)
         response_time = response.elapsed.total_seconds()
-        
-        # Print basic information
+
+        #printing health info
         print(f"Checked {api_url}: Status Code {response.status_code}, Response Time: {response_time}s")
-        
-        # Print additional health information
         print("Time of Response:", datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
         print("Request Method:", response.request.method)
         print("Response Headers:", response.headers)
         content_length = response.headers.get('Content-Length', 'N/A')
         print(f"Content Length: {content_length}")
         
-        # Check for API health
+        #api health check
         if response.status_code != 200:
             print(f"API at {api_url} is down!")
         elif response_time > max_response_time:
@@ -30,15 +28,15 @@ def check_api_health(api_url, max_response_time=2):
     except requests.exceptions.RequestException as e:
         print(f"Error checking {api_url}: {e}")
 
-# Function to monitor multiple APIs
+#function to monitor multiple APIs
 def monitor_apis(api_urls, interval=60):
     while True:
         for api_url in api_urls:
             check_api_health(api_url)
         time.sleep(interval)
 
-# List of APIs to monitor
-api_urls = ["https://github.com/anayakgit"]
+#list of APIs to monitor
+api_urls = ["https://github.com/anayakgit", "https://www.microsoft.com/en-in"]
 
-# Start monitoring
-monitor_apis(api_urls, interval=60)  # Check every 60 seconds
+#start monitoring
+monitor_apis(api_urls, interval=30)  #check every 30 seconds
